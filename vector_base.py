1a93585ff7915ace379a7fd75d793bea883cb042
Noam Drong <noam@drong.net>
2020-07-03 13:35:11 +0200

    Add optional arguments to VectorBase class

--- vector_base.py
+++ vector_base.py
@@ -12,18 +12,20 @@ class VectorBase(ABC):
     This abstract class is used to define vulnerability vectors for the AndroBugs vulnerability scanner.
     """
 
-    def __init__(self, writer: Writer, apk: APK, dalvik: DalvikVMFormat, analysis: Analysis) -> None:
+    def __init__(self, writer: Writer, apk: APK, dalvik: DalvikVMFormat, analysis: Analysis, args: any = None) -> None:
         """
         Initialize the vector class with the resources needed for analysis.
         :param writer: Output writer.
         :param apk: APK object.
         :param dalvik: Dalvik VM object.
         :param analysis: Analysis object.
+        :param args: Optional arguments.
         """
         self.writer = writer
         self.apk = apk
         self.dalvik = dalvik
         self.analysis = analysis
+        self.args = args
 
     @property
     @abstractmethod
