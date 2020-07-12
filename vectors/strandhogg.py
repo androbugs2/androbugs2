5d758b7538ada3e2a9d676656542a905816d473b
Jasper <jaspervanthuijl@gmail.com>
2020-07-12 17:30:38 +0200

    updated strandhogg2.0 vector in case launchMode is not present in activities

--- vectors/strandhogg.py
+++ vectors/strandhogg.py
@@ -13,21 +13,27 @@ class Vector(VectorBase):
     }
 
     def check_strandhogg2(self) -> None:
-        for activity in self.apk.get_all_attribute_value("activitiy", "launchMode"):
+        found = False
+        activities_launch_mode = self.apk.get_all_attribute_value("activitiy", "launchMode")
+        for activity in activities_launch_mode:
             if activity.endswith(self.LAUNCH_MODES["standard"] or self.LAUNCH_MODES["singleTop"]):
-                self.writer.startWriter("STRANDHOGG_2",
-                                        LEVEL_CRITICAL,
-                                        "Standhogg 2.0",
-                                        "This application is vulnerable to the Standhogg 2.0 vulnerability. "
-                                        "Please set activity launchModes to 'singleTask' or 'singleInstance'."
-                                        "Please see https://promon.co/strandhogg-2-0/ for more details",
-                                        ["Strandhogg"])
-                return
-        self.writer.startWriter("STRANDHOGG_2",
-                                LEVEL_INFO,
-                                "Standhogg 2.0",
-                                "This application does not seem to be vulnerable to the Standhogg 2.0 vulnerability",
-                                ["Strandhogg"])
+                found = True
+                break
+
+        if found or not activities_launch_mode:
+            self.writer.startWriter("STRANDHOGG_2",
+                                    LEVEL_CRITICAL,
+                                    "Standhogg 2.0",
+                                    "This application is vulnerable to the Standhogg 2.0 vulnerability. "
+                                    "Please set activity launchModes to 'singleTask' or 'singleInstance'."
+                                    "Please see https://promon.co/strandhogg-2-0/ for more details",
+                                    ["Strandhogg"])
+        else:
+            self.writer.startWriter("STRANDHOGG_2",
+                                    LEVEL_INFO,
+                                    "Standhogg 2.0",
+                                    "This application does not seem to be vulnerable to the Standhogg 2.0 vulnerability",
+                                    ["Strandhogg"])
 
     def analyze(self) -> None:
         self.check_strandhogg2()
