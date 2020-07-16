c3ca5f9857c2b6dc9c679c50a9f9b8323dea97c5
Noam Drong <noam@drong.net>
2020-07-16 15:29:27 +0200

    Update Xamarin framework identification

--- vectors/framework.py
+++ vectors/framework.py
@@ -6,9 +6,9 @@ class Vector(VectorBase):
     description = "Checks if a framework was used to develop the app, and if so, which one."
 
     XAMARIN_SIGNATURE = {
-        "class_name": "Lmono/MonoPackageManager;",
-        "method_name": "LoadApplication",
-        "method_descriptor": "(Landroid/content/Context; Landroid/content/pm/ApplicationInfo; [Ljava/lang/String;)V",
+        "class_name": "Lmono/android/Runtime;",
+        "method_name": "register",
+        "method_descriptor": "(Ljava/lang/String; Ljava/lang/Class; Ljava/lang/String;)V",
     }
 
     REACT_NATIVE_SIGNATURE = {
@@ -35,26 +35,17 @@ class Vector(VectorBase):
                                     ['Framework'])
 
     def check_xamarin(self) -> bool:
-        mono_pm = self.analysis.get_method_analysis_by_name(self.XAMARIN_SIGNATURE["class_name"],
-                                                            self.XAMARIN_SIGNATURE["method_name"],
-                                                            self.XAMARIN_SIGNATURE["method_descriptor"])
-        if mono_pm is None:
-            return False
-
-        em = mono_pm.get_method()
-        for idx, ins in enumerate(em.get_instructions()):
-            if ins.get_name() == "const-string" and ins.get_output() == "v0, 'xamarin-app'":
-                ins_next = em.get_instruction(idx + 1)
-                if ins_next.get_name() == "invoke-static" \
-                        and ins_next.get_output() == "v0, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V":
-                    self.writer.startWriter("FRAMEWORK",
-                                            LEVEL_NOTICE,
-                                            "App framework identification",
-                                            "Application depends on the Xamarin framework (library detected). "
-                                            "For more information about Xamarin, see: "
-                                            "https://dotnet.microsoft.com/apps/xamarin.",
-                                            ['Framework'])
-                    return True
+        if any(self.analysis.get_method_analysis_by_name(self.XAMARIN_SIGNATURE["class_name"],
+                                                         self.XAMARIN_SIGNATURE["method_name"],
+                                                         self.XAMARIN_SIGNATURE["method_descriptor"])):
+            self.writer.startWriter("FRAMEWORK",
+                                    LEVEL_NOTICE,
+                                    "App framework identification",
+                                    "Application depends on the Xamarin framework (library detected). "
+                                    "For more information about Xamarin, see: "
+                                    "https://dotnet.microsoft.com/apps/xamarin.",
+                                    ['Framework'])
+            return True
         return False
 
     def check_flutter(self) -> bool:
